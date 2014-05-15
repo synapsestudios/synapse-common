bc95f94add376fc94952762a21b2503726512476
Ben Youngblood <ben@synapsestudios.com>
2014-05-15 08:01:27 -0700

    trick browserify on hammerjs

--- loaders/hammerjs.js
+++ loaders/hammerjs.js
@@ -3,7 +3,11 @@
 var Hammer;
 
 try {
-    Hammer = require('hammerjs');
+    // You are responsible for making sure hammerjs is available in the browser
+    // if you want to use it. Browserify will not do it automatically by including
+    // this file!
+    var req = require;
+    Hammer = req('hammerjs');
 } catch (err) {
     Hammer = function() {
         return { on : function() {}, off : function() {} };
