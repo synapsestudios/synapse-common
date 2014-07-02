51286a5439493be0ef9cc00f991d41c1d8ce0d44
Paul Statezny <paul@synapsestudios.com>
2014-07-02 10:50:53 -0700

    Refs #8 - Convert request body data to URL parameters for GET requests.
    Port toQuery from underscore-contrib:util:strings.

--- http/gateway.js
+++ http/gateway.js
@@ -16,24 +16,20 @@ var HttpGateway = Extendable.extend({
         throw 'You must extend getConfig or set this.config with a hostname and port';
     },
 
-    _getRequestOptions : function(method, path)
-    {
-        var config = this.getConfig();
-
-        return {
-            hostname : config.hostname,
-            port     : config.port,
-            method   : method,
-            path     : path,
-            headers  : {
-                'Accept'       : 'application/json',
-                'Content-Type' : 'application/json'
-            }
-        };
-    },
-
+    /**
+     * Perform a request to the API
+     *
+     * @param  string  method Request method
+     * @param  string  path   Request path
+     * @param  object  data   Request data (if any)
+     * @return promise
+     */
     apiRequest : function(method, path, data)
     {
+        if (data && method === 'GET') {
+            path = path + '?' + this._toQuery(data);
+        }
+
         return Q.Promise(_.bind(function(resolve, reject) {
             var options  = this._getRequestOptions(method, path);
 
@@ -65,7 +61,7 @@ var HttpGateway = Extendable.extend({
                 reject(e);
             });
 
-            if (data) {
+            if (data && method !== 'GET') {
                 if (_.isObject(data)) {
                     data = JSON.stringify(data);
                 }
@@ -75,6 +71,76 @@ var HttpGateway = Extendable.extend({
 
             req.end();
         }, this));
+    },
+
+    _getRequestOptions : function(method, path)
+    {
+        var config = this.getConfig();
+
+        return {
+            hostname : config.hostname,
+            port     : config.port,
+            method   : method,
+            path     : path,
+            headers  : {
+                'Accept'       : 'application/json',
+                'Content-Type' : 'application/json'
+            }
+        };
+    },
+
+    /**
+     * Encode data in object format
+     *
+     * @param  object data Query data in object form
+     * @return string
+     */
+    _toQuery : function(data)
+    {
+        return this._buildParams('', data);
+    },
+
+    /**
+     * Build query parameters
+     *
+     * Taken from underscore-contrib, freely distributed under the MIT license.
+     * @link https://github.com/documentcloud/underscore-contrib/blob/master/underscore.util.strings.js
+     *
+     * @param  string prefix Prefix to add before param
+     * @param  mixed  val    Variable to parameterize
+     * @param  bool   top    Is this a recursive call to this function?
+     * @return string
+     */
+    _buildParams : function(prefix, val, top)
+    {
+        var instance = this;
+
+        if (_.isUndefined(top)) top = true;
+
+        if (_.isArray(val)) {
+            return _.map(val, function(value, key) {
+                return instance._buildParams(top ? key : prefix + '[]', value, false);
+            }).join('&');
+        } else if (_.isObject(val)) {
+            return _.map(val, function(value, key) {
+                return instance._buildParams(top ? key : prefix + '[' + key + ']', value, false);
+            }).join('&');
+        } else {
+            return this._urlEncode(prefix) + '=' + this._urlEncode(val);
+        }
+    },
+
+    /**
+     * Encode a string to be used in a URL
+     *
+     * Taken from underscore-contrib, freely distributed under the MIT license.
+     * @link https://github.com/documentcloud/underscore-contrib/blob/master/underscore.util.strings.js
+     *
+     * @param  string string String to encode
+     * @return string
+     */
+    _urlEncode : function(string) {
+        return encodeURIComponent(string).replace(/\%20/g, '+');
     }
 
 });
