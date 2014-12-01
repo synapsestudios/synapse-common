936534d2cfbdf6bc4319ee8ad834191648de3605
Zachary Chavez <zach@synapsestudios.com>
2014-12-01 10:36:56 -0700

    Refs #23 - Extract gulp test task setup into separate files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@
 
 var gulp     = require('gulp');
 var gutil    = require('gulp-util');
-var connect  = require('gulp-connect');
-var fallback = require('connect-history-api-fallback');
 
 // Require all our tasks
 require('./gulp-tasks/browserify');
+require('./gulp-tasks/preprocess');
+require('./gulp-tasks/connect');
 
 // Set default environment
 if ( ! gutil.env.env) {
@@ -25,26 +25,4 @@ gulp.task('test', ['preprocess:test', 'watchify:test', 'connect:test', 'delta:te
 
 gulp.task('delta:test', function() {
     gulp.watch(['./tests/**/*.html'], ['preprocess:test']);
-});
-
-gulp.task('preprocess:test', function() {
-    return gulp.src([
-            './__tests__/index.html',
-            './__tests__/favicon.png',
-            './node_modules/mocha/mocha.css',
-            './node_modules/mocha/mocha.js'
-        ])
-        .pipe(gulp.dest('./test'))
-        .pipe(connect.reload());
-});
-
-gulp.task('connect:test', function() {
-    return connect.server({
-        root       : 'test',
-        port       : 9001,
-        livereload : true,
-        middleware : function (connect, options) {
-            return [fallback];
-        }
-    });
 });
