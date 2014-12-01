b86be3cf3591d9bb6e49bbf9c1a81b3db7308663
Zachary Chavez <zach@synapsestudios.com>
2014-12-01 11:18:17 -0700

    Refs #23 - Convert ArrayObject tests.

--- __tests__/lib/array-object-test.js
+++ __tests__/lib/array-object-test.js
@@ -1,15 +1,15 @@
+/* global describe, it, beforeEach */
+/* jshint -W024 */ // Ignore reserved word errors
+/* jshint expr: true */
 'use strict';
 
-var componentPath      = '../../lib/array-object';
-var componentErrorPath = '../../lib/array-object-error';
+var expect = require('chai').expect;
 
-jest.dontMock(componentPath);
-jest.dontMock(componentErrorPath);
-jest.dontMock('underscore');
+var ArrayObject      = require('../../lib/array-object');
+var ArrayObjectError = require('../../lib/array-object-error');
 
 describe('array-object', function() {
-    var ArrayObject = require(componentPath),
-        arrayObject;
+    var arrayObject;
 
     beforeEach(function() {
         arrayObject = new ArrayObject();
@@ -21,49 +21,35 @@ describe('array-object', function() {
 
             arrayObject.push(value);
 
-            expect(
-                arrayObject.get(0)
-            ).toBe(value);
+            expect(arrayObject.get(0)).to.equal(value);
         });
 
         it('increments the key used for adding new elements', function () {
-            expect(
-                arrayObject.count
-            ).toBe(0);
+            expect(arrayObject.count).to.equal(0);
 
             arrayObject.push('foo');
 
-            expect(
-                arrayObject.count
-            ).toBe(1);
+            expect(arrayObject.count).to.equal(1);
 
             arrayObject.push('food');
 
-            expect(
-                arrayObject.count
-            ).toBe(2);
+            expect(arrayObject.count).to.equal(2);
 
             arrayObject.push('foosball');
 
-            expect(
-                arrayObject.count
-            ).toBe(3);
+            expect(arrayObject.count).to.equal(3);
         });
 
         it('does not re-use a key after its element has been removed', function () {
             arrayObject.push('foo');
             arrayObject.push('food');
 
-            expect(
-                arrayObject.get(1)
-            ).toBe('food');
+            expect(arrayObject.get(1)).to.equal('food');
 
             arrayObject.remove(1);
             arrayObject.push('foosball');
 
-            expect(
-                arrayObject.get(1)
-            ).toBeUndefined();
+            expect(arrayObject.get(1)).to.equal(undefined);
         });
     });
 
@@ -75,61 +61,50 @@ describe('array-object', function() {
             arrayObject.push('string');
             arrayObject.push(thirdValue);
 
-            expect(
-                arrayObject.get(2)
-            ).toBe(thirdValue);
+            expect(arrayObject.get(2)).to.equal(thirdValue);
 
             arrayObject.remove(2);
 
-            expect(
-                arrayObject.get(2)
-            ).toBeUndefined();
+            expect(arrayObject.get(2)).to.equal(undefined);
         });
     });
 
     describe('edit', function() {
-        var ArrayObjectError = require(componentErrorPath);
 
         it('edits the element at the specified index', function () {
             arrayObject.push('foo');
 
-            expect(arrayObject.get(0)).toBe('foo');
+            expect(arrayObject.get(0)).to.equal('foo');
 
             arrayObject.edit(0, 'bar');
 
-            expect(
-                arrayObject.get(0)
-            ).toBe('bar');
+            expect(arrayObject.get(0)).to.equal('bar');
         });
 
         it('throws an exception if index is not an integer', function () {
-            var invalidIndex = 'foo',
-                expectedError,
-                expectedErrorMessage;
+            var invalidIndex, expectedErrorMessage;
 
+            invalidIndex = 'foo';
             expectedErrorMessage = 'Index must be an integer, ' + invalidIndex + ' provided';
-            expectedError        = new ArrayObjectError(expectedErrorMessage);
 
             expect(function () {
-                arrayObject.edit(invalidIndex, 'bar');
-            }).toThrow(expectedError);
+                arrayObject.edit(invalidIndex, 'bar');}
+            ).to.throw(ArrayObjectError, expectedErrorMessage);
         });
 
         it('throws an exception if index has not yet been set', function() {
-            var outOfBoundsIndex = 17,
-                expectedError,
-                expectedErrorMessage;
+            var outOfBoundsIndex, expectedErrorMessage;
 
             arrayObject.push(1);
             arrayObject.push(2);
             arrayObject.push(3);
 
+            outOfBoundsIndex = 17;
             expectedErrorMessage = 'Element ' + outOfBoundsIndex + ' not yet defined in arrayObject';
-            expectedError        = new ArrayObjectError(expectedErrorMessage);
 
             expect(function () {
                 arrayObject.edit(outOfBoundsIndex, 'bar');
-            }).toThrow(expectedError);
+            }).to.throw(ArrayObjectError, expectedErrorMessage);
         });
     });
 
@@ -141,15 +116,15 @@ describe('array-object', function() {
 
             expect(
                 arrayObject.get(0)
-            ).toBe('foo');
+            ).to.equal('foo');
 
             expect(
                 arrayObject.get(1)
-            ).toBe('bar');
+            ).to.equal('bar');
 
             expect(
                 arrayObject.get(2)
-            ).toBe('baz');
+            ).to.equal('baz');
         });
     });
 
@@ -157,7 +132,7 @@ describe('array-object', function() {
         it('returns an empty array if no items pushed onto array-object', function() {
             expect(
                 arrayObject.getAsArray()
-            ).toEqual(
+            ).to.eql(
                 []
             );
         });
@@ -178,12 +153,12 @@ describe('array-object', function() {
                 'baz',
                 1,
                 2,
-                3.14159,
+                3.14159
             ];
 
             expect(
                 arrayObject.getAsArray()
-            ).toEqual(
+            ).to.eql(
                 expectedArrayValue
             );
         });
@@ -194,19 +169,11 @@ describe('array-object', function() {
             arrayObject.push('baz');
 
             // Demonstrate first that the array returned contains the value to be removed
-            expect(
-                arrayObject.getAsArray()
-            ).toContain(
-                'bar'
-            );
+            expect(arrayObject.getAsArray()).to.contain('bar');
 
             arrayObject.remove(1);
 
-            expect(
-                arrayObject.getAsArray()
-            ).not.toContain(
-                'bar'
-            );
+            expect(arrayObject.getAsArray()).not.to.contain('bar');
         });
     });
 });
