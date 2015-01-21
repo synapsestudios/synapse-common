d211fc225b0909ab8c84332d8f042787c771b718
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-01-21 14:05:47 -0700

    Refs #29 - Exchange refresh token for new access token on 401.

--- http/auth-gateway.js
+++ http/auth-gateway.js
@@ -2,14 +2,16 @@
 
 var HttpGateway = require('./gateway');
 var store       = require('store');
+var qs          = require('querystring');
+var _           = require('underscore');
 
 var HttpAuthGateway = HttpGateway.extend({
 
-    _getRequestOptions : function(method, path)
+    getRequestOptions : function(method, path, data)
     {
         var options, token;
 
-        options = HttpGateway.prototype._getRequestOptions.call(this, method, path);
+        options = HttpGateway.prototype.getRequestOptions.call(this, method, path, data);
 
         token = store.get('token');
         if (token) {
@@ -17,6 +19,44 @@ var HttpAuthGateway = HttpGateway.extend({
         }
 
         return options;
+    },
+
+    /**
+     * Handle 401 Unauthorized responses
+     *
+     * Assume that the oauth access token has expired and the refresh token
+     * needs to be exchanged for a new one.
+     */
+    handle401 : function(resolve, reject, method, path, data, headers)
+    {
+        var gateway, token, refreshData, refreshHeaders, handleSuccess, handleFailure;
+
+        gateway = this;
+        token   = store.get('token');
+
+        data = data || {};
+
+        handleSuccess = function (accessToken) {
+            token = _.extend(token, accessToken);
+
+            store.set('token', token);
+
+            gateway.apiRequest(method, path, data, headers).then(resolve, reject);
+        };
+
+        handleFailure = function (errors) {
+            // Noop
+        };
+
+        refreshData = qs.stringify({
+            client_id     : this.config.client_id,
+            grant_type    : 'refresh_token',
+            refresh_token : token.refresh_token
+        });
+
+        refreshHeaders = {'Content-Type' : 'application/x-www-form-urlencoded'};
+
+        this.apiRequest('POST', '/oauth/token', refreshData, refreshHeaders).then(handleSuccess, handleFailure);
     }
 
 });
