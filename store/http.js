0a7af545d236ecdf88b05bfaeddf4605e5cb019c
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-02-05 14:42:41 -0700

    Refs #20 - Delete no longer used files.

--- store/http.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var BaseStore    = require('./base');
-var HttpGateway  = require('../http/gateway');
-
-var HttpStore = BaseStore.extend(HttpGateway.prototype);
-
-module.exports = HttpStore;
