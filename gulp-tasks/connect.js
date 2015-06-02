928e9cd96d1d374d0f445015b80e5d78e242d297
Zachary Chavez <zach@synapsestudios.com>
2015-06-02 14:20:25 -0700

    Use port 9999 for test runner.

--- gulp-tasks/connect.js
+++ gulp-tasks/connect.js
@@ -7,7 +7,7 @@ var fallback   = require('connect-history-api-fallback');
 gulp.task('connect:test', function() {
     return connect.server({
         root       : 'test',
-        port       : 9001,
+        port       : 9999,
         livereload : true,
         middleware : function (connect, options) {
             return [fallback];
